1ac9cb00ea5c42dfb77e3dfa0c33b48d30d227b0
gerkoon <gerkoon@gmail.com>
2014-05-11 21:35:44 +0200

    header position fixed

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,16 +2,15 @@ var resurrectionDirectives = angular.module('resurrectionDirectives', []);
 
 
 resurrectionApp.directive('backButton', function(){
-    // Runs during compile
     return {
-        restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+        restrict: 'A',
         
         link: function($scope, element, attrs) {
             element.bind('click', goBack);
 
             function goBack(){
                 history.back();
-                scope.$apply();
+                //scope.$apply();
             }
             
         }
